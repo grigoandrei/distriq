c8cfff716d0cf9987444ff529f9e92077ac4c567
Andrei Grigorescu <andrei.grigorescu2017@gmail.com>
2026-05-31 21:43:15 +0200

    Implemented table for job runs

--- src/distriq/models/database.py
+++ src/distriq/models/database.py
@@ -1,7 +1,20 @@
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import String, Boolean, Integer, DateTime, func
+from sqlalchemy import String, Boolean, Integer, DateTime, func, ForeignKey, Text
+from sqlalchemy import Enum as SAEnum
 from uuid import UUID, uuid4
 from datetime import datetime
+from enum import Enum
+
+class Status(Enum):
+    PENDING = "PENDING"
+    RUNNING = "RUNNING"
+    COMPLETED = "COMPLETED"
+    FAILED = "FAILED"
+    PERMANENTLY_FAILED = "PERMANENTLY_FAILED"
+
+class Source(Enum):
+    SCHEDULED = "SCHEDULED"
+    MANUAL = "MANUAL"
 
 class Base(DeclarativeBase):
     pass
@@ -21,4 +34,19 @@ class Job(Base):
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
     updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
 
-    
+class JobRun(Base):
+    __tablename__ = "job_runs"
+
+    id:  Mapped[UUID] = mapped_column(primary_key=True, nullable=False)
+    job_id: Mapped[UUID] = mapped_column(ForeignKey("jobs.id"), nullable=True)
+    worker_id: Mapped[UUID | None] = mapped_column(ForeignKey("workers.id"), default=uuid4, nullable=True)
+    status: Mapped[Status] = mapped_column(SAEnum(Status), nullable=False)
+    source: Mapped[Source] = mapped_column(SAEnum(Source), nullable=False)
+    attempt_number: Mapped[int] = mapped_column(Integer, default=1)
+    scheduled_time: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    enqueued_time: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    start_time: Mapped[datetime | None] = mapped_column(DateTime(timezone=True),  nullable=True)
+    end_time: Mapped[datetime | None] = mapped_column(DateTime(timezone=True), nullable=True)
+    output: Mapped[str | None] = mapped_column(Text, nullable=True)
+    error: Mapped[str | None] = mapped_column(Text, nullable=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
